522838c78a9f060005a4e22629562465e7cdb951
Gary Stafford <garystafford@rochester.rr.com>
2016-12-22 00:41:34 -0500

    Adding TravisCI support

---
+++ deploy_travisci.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Deploys ZIP build artifact to GitHub (acts as pseudo binary repository)
+# (3) environment variables in .travis.yml file used here - two are encypted
+# travis encrypt GH_TOKEN=<your_token_hash> --add
+# travis encrypt COMMIT_AUTHOR_EMAIL=<your_email_here> --add
+# export GH_COLOR_ARTIFACT_REPO=github.com/<your_repo_path>.git
+
+#set -x
+
+zip -r dist-bff-0.1.${TRAVIS_BUILD_NUMBER}.zip dist/
+
+git init
+git config user.name "travis-ci"
+git config user.email "${COMMIT_AUTHOR_EMAIL}"
+
+git add *.zip
+git commit -m "Deploy Travis CI Build #${TRAVIS_BUILD_NUMBER} artifacts to GitHub"
+git push --force --quiet "https://${GH_TOKEN}@${GH_COLOR_ARTIFACT_REPO}" master:bff
