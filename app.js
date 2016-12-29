6769c0d768baaf9dee0273f9bb63c1b9cdde3b4d
Gary Stafford <garystafford@rochester.rr.com>
2016-12-29 15:56:10 -0500

    Refactor console logging

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
 var config = require('config');
+
 var port = process.env.WEB_BFF_PORT || config.get('port');
+var targetHost = process.env.API_URL || config.get('api.url');
+var targetPort = process.env.API_PORT || config.get('api.port');
 var proxy = httpProxy.createProxyServer({});
 
 // proxy.on('proxyReq', function (proxyReq, req, res, options) {
@@ -12,17 +15,17 @@ var proxy = httpProxy.createProxyServer({});
 
 var server = http.createServer(function (req, res) {
 
-  console.log('Requested URI: ' + req.url);
+  console.log('Requested endpoint: ' + req.url);
 
   proxy.web(req, res, {
     target: {
-      host: process.env.API_URL || config.get('api.url'),
-      port: process.env.API_PORT || config.get('api.port')
+      host: targetHost,
+      port: targetPort
     }
   });
 });
 
-console.log('Listening on port ' + port);
-console.log('API URL: ' + config.get('api.url'));
-console.log('API Port: ' + config.get('api.port'));
+console.log('Listening on port: ' + port);
+console.log('API URL: ' + targetHost);
+console.log('API Port: ' + targetPort);
 server.listen(port);
